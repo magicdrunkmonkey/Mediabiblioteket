cee92ec14e6d65327a4defe617910ebdc99fb2d6
George Sydby <george.sydby@gmail.com>
2020-02-07 09:25:37 +0100

    ändrat sökvägen till filerna i LibraryController så att de hittar rätt och IML fil för att testa om det hjälper de andra

---
+++ Mediabiblioteket/UnitTest/mediabiblioteket/LibraryControllerTest.java
@@ -0,0 +1,68 @@
+package mediabiblioteket;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryControllerTest {
+
+    @Test
+    void checkUserInput() {
+    }
+
+    @Test
+    void checkInputOnlyDigits() {
+    }
+
+    @Test
+    void writeToFile() {
+    }
+
+    @Test
+    void borrowMedia() {
+    }
+
+    @Test
+    void returnMedia() {
+    }
+
+    @Test
+    void checkIfBorrowerExist() {
+    }
+
+    @Test
+    void sortMedia() {
+    }
+
+    @Test
+    void getMedia() {
+    }
+
+    @Test
+    void showSelectedMediaInfo() {
+    }
+
+    @Test
+    void searchMediaAllByString() {
+    }
+
+    @Test
+    void getMediaFromSearchResult() {
+    }
+
+    @Test
+    void searchMediaTitleByString() {
+    }
+
+    @Test
+    void getBorrower() {
+    }
+
+    @Test
+    void loadBorrowedMedia() {
+    }
+
+    @Test
+    void searchBorrowed() {
+    }
+}
